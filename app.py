b120b38b3e1b48af9b15b8db85b0fd63456e51fe
SrinijaKatakam <katakamsrinija@gmail.com>
2021-07-01 16:59:26 -0500

    123

--- app.py
+++ app.py
@@ -16,14 +16,19 @@ def index():
     number_of_words = 0
     if request.method == 'POST':
         word = request.form.get('words')
+        filename = request.form.get('filename')
 
         word = word.lower()
         word = [''.join(c for c in s if c not in string.punctuation) for s in word]
         word = "".join(word)
-        with open('static/doc1.txt', "w") as output:
+        words_file = os.path.join(basedir, 'static/' + filename)
+        with open(words_file, "a") as output:
             output.write(str(word))
 
-        text_file = "doc1.txt"
+        with open(word_file, "a") as output:
+            output.write(str(word))
+
+        text_file = filename
         with open(word_file, 'rb') as fileinput:
             for line in fileinput:
                 for words in line.split():
@@ -32,6 +37,25 @@ def index():
         number_of_words = len(word1)
 
     return render_template('index.html', textfile = text_file, number = number_of_words)
+
+
+@application.route('/searchoccurances', methods = ['GET', 'POST'])
+def search_occurances():
+    wordss = []
+    if request.method == 'POST':
+        word_entered = request.form.get('word')
+        with open(word_file, 'rb') as fileinput:
+            for line in fileinput:
+                for words in line.split():
+                    wordss.append(str(words)[2:-1])
+        wordss_line = " ".join(wordss)
+        print(wordss_line)
+        res = [i for i in range(len(wordss_line)) if wordss_line.startswith(word_entered, i)]
+        print(res)
+        print(len(res))
+        total_length = len(res)
+    return render_template('index.html', occu = total_length)
+
 
 
 if __name__ == '_main_':
